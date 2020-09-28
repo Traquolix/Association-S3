377d3a06dd9881a01695ff3b93617e389385532d
Beaujoin Milo <milo.beaujoin@gmail.com>
2020-09-28 15:41:41 +0200

    Base with working calendar

--- main/main.py
+++ main/main.py
@@ -1,25 +1,80 @@
+from personnal_utils.utils import *
 import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from datetime import date
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+from tkcalendar import DateEntry
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+window = Tk()
+window.title("Welcome to TutorialsPoint")
+window.geometry('400x400')
+window.configure(background="white")
 
-    def say_hi(self):
-        print("Milo tests")
+
+# -----------------
+
+def click_into_calendar(event):
+    top = tk.Toplevel(window)
+
+    ttk.Label(top, text='Choisir la date').pack(padx=10, pady=10)
+
+    cal = DateEntry(top, width=12, background='darkblue',
+                    foreground='white', borderwidth=2, format="%d/%m/%Y")
+    cal.format_date = "%d/%m/%Y"
+    cal.pack(padx=10, pady=10)
+
+def clicked():
+    print("cc")
+
+
+# -----------------
+
+
+a = Label(window, text="Numéro du bon de commande : ").grid(row=0, column=0)
+b = Label(window, text="Nom du destinataire : ").grid(row=1, column=0)
+c = Label(window, text="Adresse du destinataire").grid(row=2, column=0)
+d = Label(window, text="Date du jour").grid(row=3, column=0)
+a1 = Entry(window).grid(row=0, column=1)
+b1 = Entry(window).grid(row=1, column=1)
+c1 = Entry(window).grid(row=2, column=1)
+
+
+d1 = DateEntry(window, locale='fr_FR', date_pattern='dd/mm/y', width=12, background='grey',
+               foreground='white', borderwidth=2)
+# d1.insert(0, date.today().strftime("%d/%m/%Y"))
+# d1.bind("<1>", click_into_calendar)
+d1.grid(row=3, column=1)
+
+
+
+
+
+# btn = ttk.Button(window, text="Submit", command=clicked).grid(row=4, column=1)
+
+
+window.mainloop()
+"""
+
+
+
+def calendar():
+    top = tk.Toplevel(root)
+
+    ttk.Label(top, text='Choose date').pack(padx=10, pady=10)
+
+    cal = DateEntry(top, width=12, background='darkblue',
+                    foreground='white', borderwidth=2)
+    cal.pack(padx=10, pady=10)
+
 
 root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+s = ttk.Style(root)
+s.theme_use('clam')
+
+ttk.Button(root, text='DateEntry', command=calendar).pack(padx=10, pady=10)
+
+root.mainloop()
+
+"""
