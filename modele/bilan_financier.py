6fd04be6240b45569ad4a5ad37ee2f6efc5fdc39
Dimitrioll <71778942+Dimitrioll@users.noreply.github.com>
2020-09-28 15:49:51 +0200

    Create bilan_financier.py

---
+++ modele/bilan_financier.py
@@ -0,0 +1,10 @@
+Groupe Discord dédier au projet de S3 :
+ Logiciel Python pour l’aide à la gestion d’une trésorerie d’association (Solen Quiniou) 
+En tant que trésorière d’une association scientifique, je dois régulièrement effectuer des tâches telles que
+l’édition de factures, de factures acquittées, de bons de commande. Actuellement, toutes ces tâches sont
+faites entièrement manuellement. Un premier projet d’application web à été réalisé l’année dernière mais
+une application Python sera plus facile à utiliser, pour ces tâches ponctuelles. Le logiciel à développer
+devra permettre d’automatiser ces tâches. Une autre partie du projet consistera à faciliter la réalisation
+du bilan financier annuel de l’association. Ces différentes tâches sont génériques et pourraient être
+appliquées à tous types d’associations scientifiques, sportives ou culturelles. C’est pourquoi le projet
+sera un projet libre et ouvert.
