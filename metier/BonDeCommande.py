5b57f1b381968d4f9cf1e0fafce034f5cc8d2f77
Thibaud Marchand <thibaudmarchand@hotmail.com>
2020-12-07 10:53:10 +0100

    1er partie

--- metier/BonDeCommande.py
+++ metier/BonDeCommande.py
@@ -1,8 +1,22 @@
+import datetime
+
 class BonDeCommande:
 
-    def __init__(self, nom):
-        self.id = BonDeCommande.genererId()
+    def __init__(self, id, nom, infoATALA1, infoATALA2, infoATALA3, infoATALA4, infoSociete1, infoSociete2, infoSociete3, date, infoDate,  ):
+        self.id = "Prendre le numéro du bon de commande"
+        # ---------------------------------------Partie en haut à gauche---------------------------------- #
+        self.infoATALA1 = "Association pour le traitement Automatique des Lagues"
+        self.infoATALA2 = "45 rue d'Ulm"
+        self.infoATALA3 = "75230 Paris Cedex 5"
+        self.infoATALA4 = "Siret : 393 902 721 00014"
+        # ---------------------------------------Partie en haut à droite---------------------------------- #
+        self.infoSociete1 = "A :            " + "Prendre le nom de l'entreprise de destination"
+        self.infoSociete2 = "Adresse :      " + "Prendre la rue de l'entreprise de destination"
+        self.infoSociete3 = "               " + "Prendre le code postal et la ville"
+        # ---------------------------------------date----------------------------------------------------- #
+        self.date = datetime.datetime.now()
+        self.infoDate = "Date :         " + date.strftime("%y/%m/%d")
+        # ------------------------------------------------------------------------------------------------ #
 
-    @staticmethod
-    def genererId():
-        return 12 #voir comment générer le numéro de facture
+
+
